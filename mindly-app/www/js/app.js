2d55cc77013c553b6a2e8cb87d5f013f94919cf1
Tomas Castro <tomas@home-pc.lan>
2014-06-10 23:38:23 -0500

    Initial configuration step one done; cleared projects jades and app.js setup; missing erase ionic completely

--- mindly-app/www/js/app.js
+++ mindly-app/www/js/app.js
@@ -9,15 +9,10 @@
 angular.module('mindlyApp', [
     'ionic',
     'underscore',
+    'ui.router',
     ])
 
-.run(['$rootScope', '$ionicPlatform', function($rootScope, $ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
+.run(['$rootScope', function($rootScope) {
     if(window.StatusBar) {
       // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
